ec891aea3a9a96cc035020b8fa8a2beef7b31524
HamedSY <hamed007.saboor@gmail.com>
2026-01-03 21:15:58 +0330

    fix: efficient the code using profiling

--- src/newcode.java
+++ src/newcode.java
@@ -2,47 +2,58 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class StringProcessor {
+public class OptimizedStringProcessor {
     private static final int DATA_SIZE = 10000;
+    private static final int BATCH_SIZE = 100;
     
     public static void main(String[] args) {
-        System.out.println("Starting string processing...");
-        List<String> results = processData();
+        System.out.println("Starting optimized string processing...");
+        List<String> results = processDataOptimized();
         System.out.println("Processing completed. Results: " + results.size());
     }
     
-    private static List<String> processData() {
-        List<String> data = generateData();
-        List<String> results = new ArrayList<>();
+    private static List<String> processDataOptimized() {
+        List<String> data = generateDataOptimized();
+        List<String> results = new ArrayList<>(DATA_SIZE);
         
-        // this part
         for (String str : data) {
-            String processed = "";
-            for (int i = 0; i < str.length(); i++) {
-                processed += str.charAt(i);
+            StringBuilder processed = new StringBuilder(str.length() * 2);
+            
+            char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                processed.append(chars[i]);
                 
-                if (i % 3 == 0) {
-                    processed += "-";
+                if (i % 3 == 0 && i != chars.length - 1) {
+                    processed.append('-');
                 }
             }
-            results.add(processed);
+            results.add(processed.toString());
         }
         
         return results;
     }
     
-    private static List<String> generateData() {
-        List<String> data = new ArrayList<>();
+    private static List<String> generateDataOptimized() {
+        List<String> data = new ArrayList<>(DATA_SIZE);
         Random random = new Random();
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] charArray = characters.toCharArray();
         
-        for (int i = 0; i < DATA_SIZE; i++) {
-            int length = 50 + random.nextInt(50); // رشته‌های 50 تا 100 کاراکتری
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < length; j++) {
-                sb.append(characters.charAt(random.nextInt(characters.length())));
+        for (int i = 0; i < DATA_SIZE; i += BATCH_SIZE) {
+            int batchEnd = Math.min(i + BATCH_SIZE, DATA_SIZE);
+            List<String> batch = new ArrayList<>(BATCH_SIZE);
+            
+            for (int j = i; j < batchEnd; j++) {
+                int length = 50 + random.nextInt(50);
+                StringBuilder sb = new StringBuilder(length);
+                
+                for (int k = 0; k < length; k++) {
+                    sb.append(charArray[random.nextInt(charArray.length)]);
+                }
+                batch.add(sb.toString());
             }
-            data.add(sb.toString());
+            
+            data.addAll(batch);
         }
         
         return data;
